47f9a13a96069b29b61dfa0edefb5ce4cfe7849c
Your Namechase <you@example.comcefeingold10@icloud.com>
2024-05-02 13:20:42 -0400

    mabye work

--- Lesson1(2)/run.py
+++ Lesson1(2)/run.py
@@ -11,9 +11,9 @@ def name(name):
 
 @bottle.route('/<name>/<number>')
 def namenumber():
-    return bottle.template('namenumber', namenumber=namenumber)
+    return bottle.template('number', number=number)
  
-number
+
 
 
 bottle.run(host='0.0.0.0', port=8090)
